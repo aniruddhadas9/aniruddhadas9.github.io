530d2994a4474b5f259ff4760820abbc950d8525
Aniruddha Das <aniruddhadas9@gmail.com>
2022-03-30 14:02:37 -0700

    - Clean summary  - Add data serice to hold all data for the resume

---
+++ source/src/app/aniruddh/interfaces/project.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  projectName: string
+  client?: string
+  applicationServer?: string
+  organization?: string
+  duration: string
+  technologyUsed: string
+  database?: string
+  description: string
+}
