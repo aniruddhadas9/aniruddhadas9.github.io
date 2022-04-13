8cedc72c74108922e804af66b796067253a7d88e
Aniruddha Das <aniruddhadas9@gmail.com>
2022-04-13 00:39:42 -0700

    - Fix work experience component and make data dynamic  - add social link and icon like linkedin, stackoverflow, github

--- source/src/app/aniruddh/components/summary/summary.component.ts
+++ source/src/app/aniruddh/components/summary/summary.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import {faStackOverflow} from "@fortawesome/free-brands-svg-icons/faStackOverflow";
+import {faPrint} from "@fortawesome/free-solid-svg-icons/faPrint";
 
 @Component({
   selector: 'ani-summary',
@@ -6,6 +10,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faStackOverflow = faStackOverflow;
+  faPrint = faPrint;
 
   constructor() {
   }
